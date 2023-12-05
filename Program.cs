e47f827bd12a55311863f604a48ce4ac2eb3321a
TERRENCE  MASHEGO <TERRENCE  MASHEGO@USERDELLFLOOR2>
2023-12-05 15:50:25 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // One way attribute way of creating an array
+            int[] luckyNumbers = { 4, 5, 8, 9 };
+            Console.WriteLine(luckyNumbers[0]);
+
+            // If you wanna manipulate it later, the 5 is the size
+            string[] friends = new string[5];
+            friends[0] = "Jim";
+            friends[1] = "John";
+            friends[2] = "Peter";
+            Console.WriteLine(friends[0]);
+
+            Console.ReadLine();
+        }
+    }
+}
+
